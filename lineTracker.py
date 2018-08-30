f3c3b2730eb0ab4546e2e376562a340b11ea5e42
Matthew Chang <matthew.chang@tufts.edu>
2018-08-30 18:10:01 -0400

    Added code to get contours but does not work yet

--- lineTracker.py
+++ lineTracker.py
@@ -4,9 +4,11 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time 
 import cv2
+import numpy as np
 
 _IMAGE_WIDTH = 640
 _IMAGE_HEIGHT = 480
+_KERNEL = np.ones((2, 2), np.uint8)
 
 # default framerate = 30fps
 with PiCamera() as camera:
@@ -20,8 +22,19 @@ with PiCamera() as camera:
     # use_video_port for rapid capture
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         image = frame.array
+        grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
-        cv2.imshow("Frame", image)
+        image = cv2.inRange(image, 0, 50)
+        cv2.morphologyEx(image, cv2.MORPH_CLOSE, _KERNEL, image)
+        
+        _, contours, _ = cv2.findContours(image, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+        
+        if len(contours) > 0:
+            maxc = max(contours, key=cv2.contourArea())
+            
+        cv2.drawContours(image, contours, -1, (0, 100, 100), -1)
+        
+        # cv2.imshow("Frame", image)
         key = cv2.waitKey(1) & 0xFF
         
         # clear current frame
